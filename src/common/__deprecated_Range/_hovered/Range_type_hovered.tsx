c98a63855acab26ff35fcee88017c5d759eeb0bc
Roman <1111990@list.ru>
2019-01-11 08:52:07 +0300

    refactor(range native): удалил компонент

--- src/common/__deprecated_Range/_hovered/Range_type_hovered.tsx
+++
@@ -1,5 +0,0 @@
-import {IClassNameProps, withBemMod} from "@bem-react/core";
-import "./Range_type_hovered.scss";
-import {cnRange} from "../RangeBase";
-
-export const RangeHovered = withBemMod<IClassNameProps>(cnRange(), {hovered: true});
--- src/common/__deprecated_Range/index.ts
+++
@@ -1,5 +0,0 @@
-import {RangeHovered} from "./_hovered/Range_type_hovered"
-import {RangeBase} from "./RangeBase"
-import {compose} from "@bem-react/core"
-
-export const Range = compose(RangeHovered)(RangeBase)
