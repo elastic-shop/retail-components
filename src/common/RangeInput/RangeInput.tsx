fcda35b5941da4e4721853c03bd8b3ab2efb12eb
Roman <1111990@list.ru>
2019-01-11 09:07:56 +0300

    reafactor(RangeInput): поменял интерфейсы

--- src/common/RangeInput/RangeInput.tsx
+++ src/common/RangeInput/RangeInput.tsx
@@ -1,20 +1,24 @@
 import * as React from "react";
-import {cn} from "@bem-react/classname";
-import {Range} from "../Range";
-import {Input} from "../Input";
+import { cn } from "@bem-react/classname";
+import { Range } from "../Range";
+import { Input } from "../Input";
 import "./RangeInput.scss";
-import {IPropsDoubleRangeController} from "../Range/RangeBase";
+import { IPropsDoubleRangeController } from "../Range/RangeBase";
+import { IClassNameProps } from "@bem-react/core";
 
 const cnPriceFilter = cn("RangeInput");
 
-export interface IPropsRangeController {
+export interface IPresenterRangeInput extends IClassNameProps {
+    hovered?: boolean;
+    focused?: boolean;
+}
+
+export interface IPropsRangeController extends IPresenterRangeInput {
     min: number;
     max: number;
     leftVal: number;
     rightVal: number;
     onChange: (data: any) => void;
-    hovered: boolean;
-    focused: boolean;
     tickSize?: number;
     baseColor?: string;
     lightColor?: string;
@@ -30,51 +34,55 @@ export default class RangeInput extends React.Component<IPropsRangeController, I
         super(props);
         this.state = {
             leftVal: props.leftVal,
-            rightVal: props.rightVal,
+            rightVal: props.rightVal
         };
         this.applyRange = this.applyRange.bind(this);
     }
+
     public componentDidUpdate(prevProps: IPropsRangeController) {
-        const { leftVal, rightVal } = this.props;
-        const { leftVal: oldLeftVal , rightVal: rightLeftVal } = prevProps;
+        const {leftVal, rightVal} = this.props;
+        const {leftVal: oldLeftVal, rightVal: rightLeftVal} = prevProps;
         if (leftVal + rightVal !== oldLeftVal + rightLeftVal) {
             this.setState({
                 leftVal,
-                rightVal,
+                rightVal
             });
         }
     }
+
     public render() {
-        const { hovered,
-                focused,
-                leftVal: pLeftVal,
-                rightVal: pRightVal,
-                tickSize,
-                min,
-                max,
-                onChange,
-                baseColor,
-                lightColor,
-                ...otherState } = this.props;
-        const { leftVal, rightVal } = this.state;
+        const {
+            hovered = false,
+            focused = false,
+            leftVal: pLeftVal,
+            rightVal: pRightVal,
+            tickSize,
+            min,
+            max,
+            onChange,
+            baseColor,
+            lightColor,
+            ...otherState
+        } = this.props;
+        const {leftVal, rightVal} = this.state;
         const inputs = [
             {
                 value: leftVal,
                 name: "leftVal",
                 postfix: "Left",
-                id: "price-left-value",
+                id: "price-left-value"
             },
             {
                 value: rightVal,
                 name: "rightVal",
                 postfix: "Right",
-                id: "price-right-value",
-            },
+                id: "price-right-value"
+            }
         ];
         return <div className={cnPriceFilter()}>
             <div className={cnPriceFilter("Inputs")}>
                 {inputs.map((input, key) => (
-                    <div className={cnPriceFilter(`Container${input.postfix}`)}  key={key}>
+                    <div className={cnPriceFilter(`Container${input.postfix}`)} key={key}>
                         <Input type="text"
                                id={input.id}
                                name={input.name}
@@ -85,7 +93,7 @@ export default class RangeInput extends React.Component<IPropsRangeController, I
                                onChange={this.onInputChange}
                                hovered={hovered}
                                checked={false}
-                            />
+                        />
                     </div>))}
             </div>
             <Range id="price-double-range"
@@ -103,34 +111,37 @@ export default class RangeInput extends React.Component<IPropsRangeController, I
                    focused={focused}/>
         </div>;
     }
+
     private onChange = (data: IPropsDoubleRangeController) => {
         this.setState(data);
     }
-    private onInputChange = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+    private onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const data: any = {};
         const value = e.currentTarget.value;
         const field = e.currentTarget.name;
-        const withoutLetters = String(value).replace( /\D/g, "");
+        const withoutLetters = String(value).replace(/\D/g, "");
         data[field] = Number(withoutLetters);
         this.onChange(data);
     }
-    private onDoubleChange = (field: Partial<keyof IPropsDoubleRangeController>, value: string ) => {
+    private onDoubleChange = (field: Partial<keyof IPropsDoubleRangeController>, value: string) => {
         const data: any = {};
-        const withoutLetters = String(value).replace( /\D/g, "");
+        const withoutLetters = String(value).replace(/\D/g, "");
         data[field] = Number(withoutLetters);
         this.onChange(data);
     }
+
     private applyRange() {
         this.props.onChange(this.normalizeValues());
     }
+
     private readonly normalizeValues = () => {
-        const { max, min } = this.props;
-        let { leftVal, rightVal } = this.state;
+        const {max, min} = this.props;
+        let {leftVal, rightVal} = this.state;
         if (leftVal < min) {
             leftVal = min;
         }
         if (leftVal > max) {
-           leftVal = min;
+            leftVal = min;
         }
         if (leftVal) {
             if (leftVal > rightVal) {
@@ -146,6 +157,6 @@ export default class RangeInput extends React.Component<IPropsRangeController, I
         if (rightVal < min) {
             rightVal = max;
         }
-        return { leftVal, rightVal };
+        return {leftVal, rightVal};
     }
 }
